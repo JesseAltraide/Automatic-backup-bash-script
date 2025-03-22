b5cea54daa41e9ce7beac19351db49cf94f35f80
JesseAltraide <jaltraide10@gmail.com>
2025-03-22 20:44:01 +0100

    Update backup.sh

--- home/jesse/backup.sh
+++ home/jesse/backup.sh
@@ -1,26 +1,35 @@
 #!/bin/bash
 
-#ask user for which script they want to back up
-#can you copy just one file.
-#create a temp directory file
-#copy the file from one place to another maybe a temp file 
+#have a current date variable
+#going to use the rsync stuff to back up files
+#check if directory exists
+#what if the user doesnt have rsync
+#rsync has to copy files that the user specifies and put it into a particular file specifically for it#
+#ensure that the user passes one argument
+#the rsync options are -avb --backupdir backup/source --delete
 
-if [ ! -d "/temp" ]
-then
-    sudo mkdir /temp
-fi
+current_date=$(date +%Y-%m-%d)
 
 if [ -z $1 ]
 then
-    echo "No input"
-    read -p "Enter a filename: " filename
-else
-    filename=$1
+    echo "No directory was entered"
+    exit 1
 fi
 
-if [ ! -f $filename ]
+if [ -d $1 ]
 then
-    echo "ERROR, the file does not exist"
+    echo "Directory doesn't exist"
+    exit 2
 fi
 
-sudo cp $filename /temp
+if ! command -v rsync &>/dev/null
+then
+    echo "rsync not installed run the command 'sudo apt install rsync' to install it"
+    exit 3
+fi
+
+mkdir -p "backup/$1/$current_date"
+
+rsync_options="-avb --backup-dir=backup/$1/$current_date --delete --dry-run"
+
+rsync $rsync_options "$1" "backup/$1" >> backup_$current_date.log
